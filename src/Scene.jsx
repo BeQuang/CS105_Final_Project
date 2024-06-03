674a0fae85e69eda9ebc12885e26e00c24309d1c
BEQUANG2409 <21522507@gm.uit.edu.vn>
2024-06-03 19:05:23 +0700

    Finish Final Project CS105

---
+++ src/Scene.jsx
@@ -0,0 +1,43 @@
+import {
+  Environment,
+  OrbitControls,
+  PerspectiveCamera,
+} from "@react-three/drei";
+import { Suspense, useEffect, useState } from "react";
+import { Car } from "./Car";
+import { Ground } from "./Ground";
+import { Track } from "./Track";
+import { Bird } from "./Bird";
+
+export function Scene({ bg }) {
+  const [thirdPerson, setThirdPerson] = useState(false);
+  const [cameraPosition, setCameraPosition] = useState([-6, 3.9, 6.21]);
+
+  useEffect(() => {
+    function keydownHandler(e) {
+      if (e.key == "k") {
+        // random is necessary to trigger a state change
+        if (thirdPerson)
+          setCameraPosition([-6, 3.9, 6.21 + Math.random() * 0.01]);
+        setThirdPerson(!thirdPerson);
+      }
+    }
+
+    window.addEventListener("keydown", keydownHandler);
+    return () => window.removeEventListener("keydown", keydownHandler);
+  }, [thirdPerson]);
+
+  return (
+    <Suspense fallback={null}>
+      <Environment files={process.env.PUBLIC_URL + bg} background={"both"} />
+
+      <PerspectiveCamera makeDefault position={cameraPosition} fov={40} />
+      {!thirdPerson && <OrbitControls target={[-2.64, -0.71, 0.03]} />}
+
+      <Ground />
+      <Track />
+      <Bird />
+      <Car thirdPerson={thirdPerson} />
+    </Suspense>
+  );
+}
